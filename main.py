8d0f40b2d26d12169e170f03fd27f80e3f3959de
Glitch (power-grass-attention) <none>
2020-03-07 22:22:24 +0000

    💶🍳 Updated with Glitch

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from twitter_scraper_fetcher import *
-from content_moderator import moderate
+from mlh_twitter_api import moderate
 import json
 import config
 import random
+from markov import*
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -22,16 +23,17 @@ def generate_message(body, methods=["POST"]):
 
   # Call get_user_tweets() from twitter_scraper_fetcher.py to scrape some tweets
   tweets = get_user_tweets(twitter_handle)
-
   try:
-    # Get a random tweet from the list of tweets
-    single_tweet = random.choice(tweets)
-    bot_answer = moderate(single_tweet)
+    clean_tweets = clean_tweets_data(tweets)
+   
+    bot_answer = generate_bot_answer(twitter_handle, question)
+   
 
     # Send the answer to the app, to display to the user
     answer = {"username": twitter_handle, "message": bot_answer}
     socketio.emit("bot answer", answer)
-  except:
+  except Exception as e :
+    print(e)
     bot_answer = "Sorry, I couldn't process that. Try again please."
     socketio.emit("error", {"username": twitter_handle, "message": bot_answer})
 
