2c6908ca95792e9b83003abc34c5bb0d33c00b33
Glitch (buttercup-darkened-brook) <none>
2020-03-07 20:16:45 +0000

    🐈🛩 Updated with Glitch

---
+++ twitter_scraper_fetcher.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+CONTENT_CLASS_NAME = "TweetTextSize"
+CONTENT_CONTAINER_TAGS = ["p"]
+EMPTY_ITEMS = [None, " ", "None"]
+TWITTER_URL = "https://twitter.com/"
+
+def get_elements(twitter_handle):
+    url = TWITTER_URL + twitter_handle
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html , features="html.parser")
+    return soup.find_all(CONTENT_CONTAINER_TAGS, attrs={"class": CONTENT_CLASS_NAME})
+    pass
+  
+    
+
+def get_user_tweets(twitter_handle):
+    elements = get_elements(twitter_handle)
+    tweets = []
+    for post in elements:
+      for text in post.contents:
+         # check if line contains real text
+         if text.string not in EMPTY_ITEMS:
+            tweets.append(text.string)
+    return tweets
+    pass
+
+def clean_tweets_data(tweets):
+  ## Nothing here yet!
+  pass
+    
+  
+            
+                
+
+    
